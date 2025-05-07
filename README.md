7793fc28a2e5c72aef9b058adbdf555d60623189
Igor <50610898+Igor3007@users.noreply.github.com>
2025-05-07 15:14:40 +0300

    deploy

---
+++ README.md
@@ -0,0 +1,10 @@
+# 07.05.2025
+# Metrium 2.0
+# Promicom Develop
+
+```
+node v12.22.12
+
+yarn install --ignore-engines
+
+```
---
+++ src/blocks/modules/special-offers/special-offers.js
@@ -0,0 +1,13 @@
+modules.define('special-offers', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
