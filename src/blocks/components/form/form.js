fb23d9e422b83b5fe275ce50f98b816a5d890019
olg <gleo@tut.by>
2025-08-21 10:13:04 +0300

    add select-range-units component

--- src/blocks/components/form/form.js
+++ src/blocks/components/form/form.js
@@ -45,15 +45,80 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }, 300);
 
-    document.querySelectorAll('form')?.forEach(form => {
-        form.addEventListener('reset', (e) => {
-            form.querySelectorAll('.district-selector').forEach(el => {
-                el.querySelector('input').value = "[]";
-                const container = el.querySelector("span");
-                const {placeholder} = el.dataset;
-                container.innerHTML = placeholder;
+    document
+        .querySelectorAll('form')
+        .forEach(form => {
+            form.addEventListener('reset', (e) => {
+                form.querySelectorAll('.district-selector').forEach(el => {
+                    el.querySelector('input').value = "[]";
+                    const container = el.querySelector("span");
+                    const {placeholder} = el.dataset;
+                    container.innerHTML = placeholder;
+                });
             });
         });
+
+
+    /* SELECT-RANGE-UNITS */
+
+    document.querySelectorAll(".select-units").forEach(select => {
+        const _head = select.querySelector('._head');
+        const _body = select.querySelector('._body');
+        const _from = select.querySelector('._from input');
+        const _to = select.querySelector('._to input');
+
+        const {placeholder} = select.dataset;
+
+        let values = {
+            from: undefined,
+            to: undefined
+        };
+
+        const closeHandler = (e) => {
+            if (_head.contains(e.target)) { return }
+            if (!_body.contains(e.target)) {
+                document.removeEventListener('click', closeHandler);
+                select.classList.remove('active');
+            }
+        }
+
+        const renderValues = () => {
+            let unitEl = select.querySelector('._units input:checked');
+            const {unit} = unitEl.dataset;
+            let str = '';
+            str += (values.from) ? `от ${values.from} ` : '';
+            str += (values.to) ? `до ${values.to} ` : '';
+            str += (str) ? unit : placeholder;
+            _head.querySelector('span').innerHTML = str;
+        }
+
+        _head?.addEventListener('click', (e) => {
+            select.classList.toggle('active');
+            if (select.classList.contains('active')) {
+                document.addEventListener('click', closeHandler);
+            } else {
+                document.removeEventListener('click', closeHandler);
+            }
+        });
+
+        _from?.addEventListener('blur', (e) => {
+            const {value} = e.target;
+            values.from = parseInt(value.replace(/\D/g,'')) || '';
+            e.target.value = values.from;
+            e.target.dispatchEvent(new Event('change'));
+        });
+
+        _to?.addEventListener('blur', (e) => {
+            const {value} = e.target;
+            values.to = parseInt(value.replace(/\D/g,'')) || '';
+            e.target.value = values.to;
+            e.target.dispatchEvent(new Event('change'));
+        });
+
+        select.querySelectorAll('input').forEach(input => {
+           input.addEventListener('change', renderValues);
+        });
+
     });
 
 });
--- src/js/vendor/af-select.js
+++ src/js/vendor/af-select.js
@@ -316,10 +316,10 @@ export class afSelect {
     closeSelect() {
         if (!document.querySelector('.select-styled.active')) return false
 
-        document.querySelector('.select-styled.active').classList.remove('active')
-        document.querySelector('.select-options.active').classList.remove('active')
-        document.querySelector('.select-list.active').classList.remove('active')
-        document.querySelector('body').classList.remove('af-select-open')
+        document.querySelector('.select-styled.active')?.classList.remove('active')
+        document.querySelector('.select-options.active')?.classList.remove('active')
+        document.querySelector('.select-list.active')?.classList.remove('active')
+        document.querySelector('body')?.classList.remove('af-select-open')
     }
 
     clickEventOut() {
