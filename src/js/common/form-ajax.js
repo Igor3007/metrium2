d1702aec48b06b1719c283cfd2c476cbd192ac7f
Danilkin <danilkin.sergey@gmail.com>
2025-08-27 16:31:07 +0300

    change form ajax

--- src/js/common/form-ajax.js
+++ src/js/common/form-ajax.js
@@ -2,26 +2,53 @@ export const initFormAJAX = () => {
     document
         .querySelectorAll('form[data-send="ajax"]')
         .forEach((form) => {
-            form.addEventListener("submit", e => {
+            form.addEventListener("submit", async (e) => {
                 e.preventDefault();
                 const url = form.getAttribute("action");
                 const formData = new FormData(form);
                 const data = Object.fromEntries(formData);
-                fetch(url, {
-                    method: "POST",
-                    body: data
-                })
-                    .then(res => {
-                        if (res.ok) {
-                            window.STATUS.msg(successMsg)
-                        } else {
-                            window.STATUS.err(res.statusText);
-                        }
-                    })
-                    .catch(err => {
-                        console.error(err);
-                        window.STATUS.err('Извините, что-то пошло не так…');
+
+                try {
+
+                    const response = await fetch(url, {
+                        method: "POST",
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            'X-CSRF-TOKEN': data._token // берем токен из формы
+                        },
+                        body: JSON.stringify(data)
                     });
+
+                    const result = await response.json();
+
+                    if(response.ok && result.success)
+                    {
+                        form.reset();
+                        window.STATUS.msg('Форма успешно отправлена');
+                        return;
+                    }
+
+                    //Ошибка валидации (422)
+                    if (response.status === 422) {
+                        window.STATUS.err(result.error);
+                        return;
+                    }
+
+                    //Ограничение частоты запросов (429)
+                    if (response.status === 429) {
+                        window.STATUS.err('Слишком много запросов! Попробуйте позже');
+                        return;
+                    }
+
+                    //Любая другая ошибка (500 и т.п.)
+                    console.error(err);
+                    window.STATUS.err('Извините, что-то пошло не так…');
+                }
+                catch (err) {
+                    console.error(err);
+                    window.STATUS.err('Извините, что-то пошло не так…');
+                }
             })
 
         })
